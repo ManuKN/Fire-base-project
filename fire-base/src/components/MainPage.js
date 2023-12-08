f85a9affb5514b1f8f3124b4703f492270486782
Manu K N <201910101325@presidencyuniversity.in>
2023-12-08 18:15:19 +0530

    Added Header succusfully

---
+++ fire-base/src/components/MainPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './MainPage.css'
+
+export default function MainPage() {
+  return (
+   <header className='nav_bar1'>
+    <nav className=''>
+    <ul>
+      <li><a href='https://javascript.info/async-await'>Home</a></li>
+      <li><a href='https://javascript.info/async-await'>Carrer</a></li>
+      <li><a href='https://javascript.info/async-await'>Account</a></li>
+      <li><a href='https://javascript.info/async-await'>Feedback</a></li>
+      <li><a href='https://javascript.info/async-await'>Contact</a></li>
+    </ul>
+    </nav>
+   </header>
+  )
+}
